394bbd7ab28b7b8de2bd2b5e9a2507e3aab3ca51
marinashtrassheim <73815942+marinashtrassheim@users.noreply.github.com>
2025-01-07 11:26:18 +0500

    Delete 6.2Regex.py

--- 6.2Regex.py
+++
@@ -1,7 +0,0 @@
-import re
-some_string = 'Напишите функцию функцию, которая будет будет будет будет\nудалять все все все все последовательные повторы слов из из из из заданной\nстроки строки при помощи регулярных выражений'
-def remove_repetition(text):
-  return re.sub(r'\b(\w+)(\s+\1)+\b', r'\1', text)
-result = remove_repetition(some_string)
-print(result)
-
